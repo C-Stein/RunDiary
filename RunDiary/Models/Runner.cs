22725843fd76edb723bff8d29d3bc924c1e58990
C-Stein <caitlin.teter@gmail.com>
2015-12-08 15:12:58 -0600

    added some basic info to Runner.cs

--- RunDiary/Models/Runner.cs
+++ RunDiary/Models/Runner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,20 @@ namespace RunDiary.Models
 {
     public class Runner
     {
+        [Key]
+        public int RunnerId { get; set; }
+
+        //public virtual ApplicationUser RealUser { get; set; }
+
+        [MaxLength(20)]
+        [MinLength(3)]
+        [RegularExpression(@"^[a-zA-Z\d]+[-_a-zA-Z\d]{0,2}[a-zA-Z\d]+")]
+        public string Handle { get; set; }
+
+        // ICollection, IEnumerable, IQueryable
+        public List<Run> Runs { get; set; }
+        public List<Race> Races { get; set; }
+        public List<Run> FavoriteRuns { get; set;}
+
     }
 }
