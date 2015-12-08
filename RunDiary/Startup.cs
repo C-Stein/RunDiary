e677b8e055c328f5699249822ba20a9fac32d4c9
C-Stein <caitlin.teter@gmail.com>
2015-12-08 14:38:31 -0600

    initial commit

---
+++ RunDiary/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RunDiary.Startup))]
+namespace RunDiary
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
