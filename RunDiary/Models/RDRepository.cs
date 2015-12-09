d97518175de4e818aa027e248f9a1a8af11f222a
C-Stein <caitlin.teter@gmail.com>
2015-12-09 15:25:41 -0600

    combined Race.cs and Run.cs, strted building RDContext

--- RunDiary/Models/RDRepository.cs
+++ RunDiary/Models/RDRepository.cs
@@ -7,5 +7,17 @@ namespace RunDiary.Models
 {
     public class RDRepository
     {
+        private RDContext _context;
+        public RDContext Context {  get { return _context; } }
+
+        public RDRepository()
+        {
+            _context = new RDContext();
+        }
+
+        public RDRepository(RDContext a_context)
+        {
+            _context = a_context;
+        }
     }
 }
